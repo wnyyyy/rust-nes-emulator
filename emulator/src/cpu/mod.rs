d4bf3abd17cc1ebb41ba1affd14702388f70bd9a
wny <viniberizzi@gmail.com>
2023-12-17 14:04:25 -0300

    add addressing modes immediate, zeropage and absolute

--- emulator/src/cpu/mod.rs
+++ emulator/src/cpu/mod.rs
@@ -1,6 +1,7 @@
 use crate::common::errors::EmulatorError;
 use crate::cpu::opcode::{get_opcode};
-use crate::cpu::types::ProcessorStatus;
+use crate::cpu::types::{AddressingMode, ProcessorStatus};
+use crate::memory::cpu_memory_map::CpuMemoryMap;
 
 mod types;
 mod instructions;
@@ -8,12 +9,13 @@ mod tests;
 pub mod opcode;
 
 pub struct CPU {
-    pub program_counter: u16,
-    pub stack_pointer: u8,
-    pub register_a: u8,
-    pub register_x: u8,
-    pub register_y: u8,
-    pub status: ProcessorStatus,
+    program_counter: u16,
+    stack_pointer: u8,
+    register_a: u8,
+    register_x: u8,
+    register_y: u8,
+    status: ProcessorStatus,
+    memory: CpuMemoryMap,
 }
 
 impl CPU {
@@ -25,14 +27,20 @@ impl CPU {
             register_x: 0,
             register_y: 0,
             status: ProcessorStatus::new(),
+            memory: CpuMemoryMap::new(),
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) -> Result<(), EmulatorError>{
+    pub fn load_and_run(&mut self, program: Vec<u8>) -> Result<(), EmulatorError> {
+        self.memory.write_program(program);
+        self.run()
+    }
+
+    pub fn run(&mut self) -> Result<(), EmulatorError>{
         self.program_counter = 0;
 
         loop {
-            let opcode_u8 = program[self.program_counter as usize];
+            let opcode_u8 = self.memory.read(self.program_counter)?;
             let opcode= get_opcode(opcode_u8).ok_or(EmulatorError::InvalidOpcode(opcode_u8))?;
             self.program_counter += 1;
 
@@ -61,5 +69,36 @@ impl CPU {
             }
         }
         Ok(())
+    }
+
+    fn get_param_address(&self, mode: AddressingMode) -> Result<(u16), EmulatorError> {
+        match mode {
+            AddressingMode::Immediate => Ok(self.program_counter),
+            AddressingMode::ZeroPage => {
+                let address = self.memory.read(self.program_counter)?;
+                Ok(address as u16)
+            }
+            AddressingMode::ZeroPageX => {
+                let address = self.memory.read(self.program_counter)?;
+                Ok((address.wrapping_add(self.register_x)) as u16)
+            }
+            AddressingMode::ZeroPageY => {
+                let address = self.memory.read(self.program_counter)?;
+                Ok((address.wrapping_add(self.register_y)) as u16)
+            }
+            AddressingMode::Absolute => {
+                let address = self.memory.read_little_endian(self.program_counter)?;
+                Ok(address)
+            }
+            AddressingMode::AbsoluteX => {
+                let address = self.memory.read_little_endian(self.program_counter)?;
+                Ok(address.wrapping_add(self.register_x as u16))
+            }
+            AddressingMode::AbsoluteY => {
+                let address = self.memory.read_little_endian(self.program_counter)?;
+                Ok(address.wrapping_add(self.register_y as u16))
+            }
+
+        }
     }
 }
