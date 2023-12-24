7ca700f250360a29e0087bedcfa5caeb78dda653
wny <viniberizzi@gmail.com>
2023-12-24 08:41:45 -0300

    add debug

--- emulator/src/common/constants.rs
+++ emulator/src/common/constants.rs
@@ -4,3 +4,5 @@ pub const MEMORY_SIZE: usize = 65536;
 pub const STACK_START: u16 = 0x0100;
 pub const STACK_POINTER_INIT: u8 = 0xFF;
 pub const IRQ_VECTOR: u16 = 0xFFFE;
+
+pub static DEBUG: bool = true;
