19608bebf06923d7b615e5ba009da1e6760d81eb
wny <viniberizzi@gmail.com>
2023-12-24 22:06:43 -0300

    add snake game aliases for debugging

--- emulator/src/common/constants.rs
+++ emulator/src/common/constants.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
 pub const RAM_SIZE: u16 = 2048;
 pub const MEMORY_SIZE: usize = 65536;
 
@@ -6,3 +9,23 @@ pub const STACK_POINTER_INIT: u8 = 0xFF;
 pub const IRQ_VECTOR: u16 = 0xFFFE;
 
 pub static DEBUG: bool = true;
+
+
+lazy_static! {
+    pub static ref SNAKE_GAME_ALIAS: HashMap<u16, &'static str> = {
+        let mut map = HashMap::new();
+        map.insert(0x606, "init");
+        map.insert(0x60D, "initSnake");
+        map.insert(0x62A, "generateApplePosition");
+        map.insert(0x638, "loop");
+        map.insert(0x64D, "readKeys");
+        map.insert(0x68D, "checkCollision");
+        map.insert(0x694, "checkAppleCollision");
+        map.insert(0x6A8, "checkSnakeCollision");
+        map.insert(0x735, "gameOver");
+        map
+        };
+}
+pub fn get_alias(address: u16) -> &'static str {
+    SNAKE_GAME_ALIAS.get(&address).unwrap_or(&"")
+}
